bf49dff51136aa3f5f1e7c018e82c47508aec7ab
Gaurav Khatri <gauravkhatriweb@gmail.com>
2026-08-06 11:46:25 +0500

    feat: add documentation and examples for Python variables and naming conventions in Day 01 script

--- 90_Days_System/Month_1/Week_1/Daily/Day_01.py
+++ 90_Days_System/Month_1/Week_1/Daily/Day_01.py
@@ -1,6 +1,6 @@
 """
 Day 1 — Python Basics & Data Structures
-Date: [Fill Date]
+Date: [5-7-2026]
 
 Topic: Python Basics & Data Structures
 Objectives:
@@ -15,3 +15,38 @@ Notes:
 # Your code goes below
 # ---
 print("Hello, Day 1!")
+
+
+#Comments 
+#Single Line Comments 
+#Helloo 
+# Multline Comments 
+"""Hello 
+This 
+is 
+dogstring 
+"""
+
+
+#Variables 
+
+#Varibles are used to store any thing in python. 
+name= "Gaurav Khatri" 
+#Rules while making a variables 
+#1: You canot start a varibles with a number eg:1name 
+#2: You canot add space in a varibles name 
+#3: You cannot use special characters in a variables name
+
+
+#Naming Convention:
+#1: Camel Case: 
+userName = "Gaurav Khatri"
+#2: Pascal Case:
+UserName = "Gaurav Khatri"
+#3: Snake Case:
+user_name="Gaurav Khatri"
+
+#I am foucing on the video now not on the notes beacuse it is easy and very basic for me. 
+
+
+
